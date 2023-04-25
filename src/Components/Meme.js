8528f6805fac0141aac13c827e0f01bea1c408ae
Rajnish <b421037@iiit-bh.ac.in>
2023-04-25 08:21:31 +0530

    useEffect updated with async & await

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -8,10 +8,13 @@ export default function Meme() {
   });
   const [allMemes, setAllMemes] = React.useState([]);
 
-  React.useEffect(function () {
-    fetch(`https://api.imgflip.com/get_memes`)
-      .then((res) => res.json())
-      .then((data) => setAllMemes(data.data.memes));
+  React.useEffect(() => {
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = res.json();
+      setAllMemes(data.data.memes);
+    }
+    getMemes();
   }, []);
 
   function getMemeImage() {
